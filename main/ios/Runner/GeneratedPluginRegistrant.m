3d111e18c4afd317e173967b8c85e2e92f415e45
team9797 <104425264+team9797@users.noreply.github.com>
2022-05-24 00:16:16 +0300

    Add files via upload

---
+++ main/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<barcode_scanner/ScanbotBarcodeSdkPlugin.h>)
+#import <barcode_scanner/ScanbotBarcodeSdkPlugin.h>
+#else
+@import barcode_scanner;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ScanbotBarcodeSdkPlugin registerWithRegistrar:[registry registrarForPlugin:@"ScanbotBarcodeSdkPlugin"]];
+}
+
+@end
